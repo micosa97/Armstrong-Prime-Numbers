da5d091ddbebdf093888fb0b26f3c4b853ea0c5e
Michał Osadnik <micosa97@gmail.com>
2017-10-09 20:37:36 +0200

    Update main.rb

--- main.rb
+++ main.rb
@@ -16,7 +16,7 @@ class Main
   pot=[]
   power(pot) #oblicznie poteg aby potem korzystać z nich w czasie 1
   for i in (1..23)
-    Generating.new.generuj(i, i, 0, 0, 0, pot) #multipodzbiory zbioru cyfr o dlugosci i, zostalo jeszcze i miejsc, oraz max used val=0 oraz na razie suma to 0, a liczba już stworzona to tez 0
+    Generating.new.generate(i, i, 0, 0, 0, pot) #multipodzbiory zbioru cyfr o dlugosci i, zostalo jeszcze i miejsc, oraz max used val=0 oraz na razie suma to 0, a liczba już stworzona to tez 0
 
   end
 end
